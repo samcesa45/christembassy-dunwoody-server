29505703d6176805e12008e64d5018cf25d5ca42
samcesa45 <samcesa45@gmail.com>
2025-09-15 18:09:22 +0100

    minor fix

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,6 +1,5 @@
 export default () => ({
     database: {
-        url: process.env.DATABASE_URL || 
-        'postgresql://macbook@localhost:5432/dunwoodchurchdb?schema=public',
+        url: process.env.DATABASE_URL 
     }
 })
