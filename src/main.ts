e6868125a846bef91dc6f42c59a913ab8831e6cd
samcesa45 <samcesa45@gmail.com>
2025-09-15 18:39:38 +0100

    added port

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
       limit: '1mb'
     })
   )
-  const port = process.env.PORT ?? 3000
+  const port = process.env.PORT ?? 4000
   await app.listen(port);
   console.log(`Server listening on ${port}`);
 
